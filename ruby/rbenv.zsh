29b67fe3dc7ac63d52781913638cb6f007ffa77c
Mark Alston <mark_alston@yahoo.com>
2014-11-23 21:21:24 -0700

    Updated rbenv

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,4 +1,6 @@
 # init according to man page
+# To use Homebrew's directories rather than ~/.rbenv add to your profile:
+export RBENV_ROOT=/usr/local/var/rbenv
 if (( $+commands[rbenv] ))
 then
   eval "$(rbenv init -)"
